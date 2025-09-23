b10c4c7ddc0c52689171279b798f91dad71a7170
rahuldogney02 <rahuldogney02@gmail.com>
2025-09-24 00:25:54 +0530

    Revert "Merge pull request #7 from rahuldogney02/feature/app-layout"
    This reverts commit 80a2141b51be61260ef716cebf8529c93989316a, reversing
    changes made to 686dba9a29728b109251063d9bfc893a7695ce48.

--- frontend/lib/fontawesome.js
+++
@@ -1,7 +0,0 @@
-// FontAwesome configuration for optimal tree-shaking
-import { config } from '@fortawesome/fontawesome-svg-core';
-
-// Prevent FontAwesome from adding its CSS since we want to control it
-config.autoAddCss = false;
-
-export default config;
